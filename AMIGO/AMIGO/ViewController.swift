1b5fabf161365ae9341156b792a097269b4120c3
Amogh <102756648+jamogh@users.noreply.github.com>
2024-01-04 00:00:32 +0530

    Add files via upload

---
+++ AMIGO/AMIGO/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AMIGO
+//
+//  Created by user1 on 03/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    
+    
+    
+    
+}
+
