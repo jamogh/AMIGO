4cdde8ac576dd08abfeccac96ffba426ecb1e189
Amogh <102756648+jamogh@users.noreply.github.com>
2024-02-23 16:23:54 +0530

    Add files via upload

--- AMIGO/AMIGO/Chat/chatViewController.swift
+++ AMIGO/AMIGO/Chat/chatViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class chatViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +22,9 @@ class chatViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func enterButton(_ sender: Any) {
+        textView.text = textField.text!
     }
-    */
+    
 
 }
